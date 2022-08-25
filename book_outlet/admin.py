646a02d023c0017662f0bca64d448496c0294be9
Deepak Jangir <deepak@Deepaks-MacBook-Pro.local>
2022-08-25 13:13:42 +0100

    Django Admin Changes

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+
+from .models import Book
 # Register your models here.
+
+
+## Use this BookAdmin class to set various setting, to overwrite various properties in this class
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ('slug',)
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author','rating')
+    list_display = ('title','author')
+
+
+admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     author = models.CharField(null=True,max_length=100)
     is_bestselling = models.BooleanField(default=False)
 
-    slug = models.SlugField(default='',null=False, db_index=True)
+    slug = models.SlugField(default='',null=False, db_index=True,blank=True)
 
     def get_absolute_url(self):
         return reverse("book-detail", args = [self.slug])
